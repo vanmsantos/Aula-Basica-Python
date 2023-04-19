b8ef2eb20ae71727292202d5ddcdd51116a54a57
Vania M Santos <102741959+vanmsantos@users.noreply.github.com>
2023-04-19 20:11:20 +0200

    teste commit

---
+++ Aula1PythonBasico.py
@@ -0,0 +1,35 @@
+print('ola mundo! Aula 1 de Phyton!')
+
+print('Basico')
+print('Meu primeiro comando')
+
+#isso e um comentario para explicar meu codigo
+print('Vania Girl Power')
+
+#calculos operadores em python
+print(8+8)
+print(18/8)
+print(18//8)
+print(4+8)
+print(5%2)
+
+#variavel em python
+
+a=5
+b=3.5
+c= 'vania'
+
+soma = a + b
+type(c)
+type(b)
+
+print (soma)
+print ('o resultado da soma e:', soma)
+
+#variavel pra conversar com usuairo 
+
+nome = input ('Qual o seu nome?')
+print(nome)
+
+idade = input ('Qual sua idade ?')
+print(idade)
